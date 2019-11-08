071be5d61df625fb118d3bd52c0adf4c928a9c0d
Matthew Coleman <K31073@eltham-college.org.uk>
2019-11-08 12:54:44 +0000

    Add project files.

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,42 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Bank_Account
+    {
+        private float Balance;
+
+        private string user;
+
+        public void accountinfo(string name, float initBalance)
+        {
+            user = name;
+            Balance = initBalance;
+
+        }
+        public void Deposit(float deposit)
+        {
+            Balance = Balance + deposit;
+        }
+        public void withdraw(float withdrawal)
+        {
+            Balance = Balance - withdrawal;
+        }
+        public void 
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+    
+}*/
